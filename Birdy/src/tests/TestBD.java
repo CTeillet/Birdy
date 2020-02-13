eb13e6409fb5420782e0200259be88fc21283b30
Teillet Corentin <teillet.corentin@neuf.fr>
2020-02-13 15:43:22 +0100

    avancement bd

--- Birdy/src/tests/TestBD.java
+++ Birdy/src/tests/TestBD.java
@@ -1,75 +1,25 @@
 package tests;
 
-import java.io.PrintStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
+import javax.naming.*;
+import javax.sql.*;
+import java.sql.*;
 
 public class TestBD {
-	String url = "jdbc:localhost:Corentin_Teillet";
-	String user = "root"; // login
-	String password = "root"; // mot de passe
-	Connection connexion = null;
-	PrintStream out = System.out;
-	
-	public static void main(String in[]) {
-	TestBD c = new TestBD();
-	String sql = in[0];
-	c.traiteRequete(sql);
-	}
-	public TestBD(){
-		try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch(Exception e) {
-			gestionDesErreurs(e);
-		}
-	}
-	
-	public void traiteRequete(String requete) {
-		out.println(requete);
-		try {
-			connexion = DriverManager.getConnection(url, user, password);
-			Statement lecture = connexion.createStatement();
-			ResultSet curseur = lecture.executeQuery(requete);
-			ResultSetMetaData infoResultat = curseur.getMetaData();
-			int nbChamp =infoResultat.getColumnCount();
-			out.println("le resultat de la requete est:");
-			String entete = "";
-			for(int i=1; i<= nbChamp; i++) {
-				String nomChamp = infoResultat.getColumnName(i);
-				entete = entete + nomChamp + "\t";
-			}
-			out.println(entete);
-			out.println("-----------------------------");
-			/* affichage des tuples */
-			while (curseur.next()) {
-				String tuple = "";
-				for(int i=1; i<= nbChamp; i++) {
-					String valeurChamp = curseur.getString(i);
-					tuple = tuple + valeurChamp + "\t";
+	public void init() {
+		try{
+			Context  ctx = new InitialContext();
+			if(ctx == null ) throw new Exception("Boom");
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/db");
+			if (ds != null) {
+				Connection conn = ds.getConnection();
+				if(conn != null) {
+					Statement stmt = conn.createStatement();
+					ResultSet rst = stmt.executeQuery("select ...");
+					if(rst.next()) {
+						}
+					conn.close();
 				}
-				out.println(tuple);
-			}
-			curseur.close();
-			lecture.close();
-			connexion.close();
-		}catch(Exception e) {
-			gestionDesErreurs(e);
+			} catch(Exception e) { e.printStackTrace(); }
 		}
-	} /* fin traiteRequete */
-	
-	protected void gestionDesErreurs(Exception e) {
-		out.println("Exception: " + e);
-		e.printStackTrace();
-		try {
-		if (connexion != null)
-		connexion.close();
-		}catch(Exception se) {
-		out.println("Tout autre probleme: " + se);
-		} throw new RuntimeException("Arret immediat");
-		}
-
-
+	}
 }
