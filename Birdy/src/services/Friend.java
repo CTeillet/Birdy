5e63b85e6721e574e52d38191e10e24a6c75f2b0
Corentin Teillet <teillet.corentin@neuf.fr>
2020-02-14 22:39:17 +0100

    debut servlets friend

--- Birdy/src/services/Friend.java
+++ Birdy/src/services/Friend.java
@@ -12,12 +12,20 @@ public class Friend {
 	}
 	
 	public static JSONObject addFriend(String id1, String id2) {
-		
+		if(id1==null || id2==null || id1==id2) {
+			return tools.ErrorJSON.serviceRefused("Identifiant non defini", -1);
+		}else {
+			
+		}
 		return null;
 	}
 	
 	public static JSONObject removeFriend(String id1, String id2) {
-		
+		if(id1==null || id2==null || id1==id2) {
+			return tools.ErrorJSON.serviceRefused("Identifiant non defini", -1);
+		}else {
+			
+		}
 		return null;
 	}
 }
