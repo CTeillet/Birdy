358ba38c4c1bc944a352baf2bd8d1f19ca87e8e4
Teillet Corentin <teillet.corentin@neuf.fr>
2020-02-21 14:48:26 +0100

    fin servlets Users

--- Birdy/src/bd/DBStatic.java
+++ Birdy/src/bd/DBStatic.java
@@ -5,6 +5,6 @@ public class DBStatic {
 	public static final String mongo_bd = "Message";
 	public static final String mysql_host = "localhost";
 	public static final String mysql_db = "Corentin_Teillet";
-	public static final String mysql_user = "phpmyadmin";
-	public static final String mysql_password = "nono9196" ;
+	public static final String mysql_user = "root";
+	public static final String mysql_password = "root" ;
 }
--- Birdy/src/servlets/User.java
+++ Birdy/src/servlets/User.java
@@ -7,22 +7,54 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
+import tools.JSONTools;
+
 public class User extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		String param = req.getPathInfo();
+		String[] sep = param.split("/");
+		JSONObject retour = null;
+		if(sep.length == 0 || sep==null) { 
+			retour = services.User.getUserList();
+		}else {
+			if(sep.length == 2) {
+				retour = services.User.getUser(sep[1]);
+			}
+		}
+		resp.setContentType("application/json");
+		resp.getWriter().print(retour);
 	}
 	
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doDelete(req, resp);
+		String param = req.getPathInfo();
+		String[] sep = param.split("/");
+		JSONObject retour = null;
+		if(sep.length == 0 || sep==null) { 
+			retour = JSONTools.serviceRefused("Pas d'argument", -1);
+		}else {
+			if(sep.length == 2) {
+				retour = services.User.deleteUser(sep[1]);
+			}
+		}
+		resp.setContentType("application/json");
+		resp.getWriter().print(retour);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		String id=null, mdp=null, mail=null, nom=null, prenom=null, dateNaissance=null, temp=null;
+		if((temp=req.getParameter("id")) !=null) id=temp;
+		if((temp=req.getParameter("mdp")) !=null) mdp=temp;
+		if((temp=req.getParameter("mail")) !=null) mail=temp;
+		if((temp=req.getParameter("nom")) !=null) nom=temp;
+		if((temp=req.getParameter("prenom")) !=null) prenom=temp;
+		if((temp=req.getParameter("dateNaissance")) !=null) dateNaissance=temp;
+		JSONObject retour = services.User.createUser(id, mdp, mail, nom, prenom, dateNaissance);
+		resp.setContentType("application/json");
+		resp.getWriter().print(retour);
 	}
 }
